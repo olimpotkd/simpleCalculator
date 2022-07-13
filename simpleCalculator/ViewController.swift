be2425b64dbbea7af6c8931dd209a154e9876391
Carlos <olimpotkd@gmail.com>
2022-07-12 20:25:50 -0400

    Initial project files. Adds buttons, and base layout. Adds numericButtonPressed handler function.

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var displayingStringValue: String = ""
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numericButtonPressed(_ sender: UIButton) {
+        if let buttonValue = sender.titleLabel?.text {
+            print(buttonValue)
+            displayingStringValue += buttonValue
+            textField.text = displayingStringValue
+        }
+        
+    }
+    
 }
 
