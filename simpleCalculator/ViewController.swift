811f2cb16cf541fc0484024688ddeda45f6082e1
Carlos <olimpotkd@gmail.com>
2022-07-17 19:46:37 -0400

    Adds numeric button press handler, and logic. Adds operator button press and logic. Add logic to not show trailing zeroes when applicable

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var displayingStringValue: String = ""
-    @IBOutlet weak var textField: UITextField!
+    var displayingStringValue: String = "0"
+    var operationValue: Double = 0
+    var lastOperationValue: Double = 0
+    var result: Double = 0
+    var shouldClear: Bool = false
+    
+    @IBOutlet weak var valueDisplay: UILabel!
+    
+    enum operationType : String {
+        case sum = "+"
+        case substract = "-"
+        case divide = "/"
+        case multiply = "*"
+        case none = ""
+    }
+    
+    
+    var queuedOperation: operationType = operationType.none
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +33,118 @@ class ViewController: UIViewController {
     }
     
     @IBAction func numericButtonPressed(_ sender: UIButton) {
-        if let buttonValue = sender.titleLabel?.text {
-            print(buttonValue)
-            displayingStringValue += buttonValue
-            textField.text = displayingStringValue
+        if shouldClear {
+            clearDisplayValue()
         }
+        if let buttonValue = sender.titleLabel?.text {
+            if buttonValue == "." && displayingStringValue.contains(".") {
+                return
+            }
+            if displayingStringValue == "0" && buttonValue != "." {
+                displayingStringValue = buttonValue
+            } else {
+                displayingStringValue += buttonValue
+            }
+            valueDisplay.text = displayingStringValue
+        }
+        operationValue = Double(displayingStringValue) ?? 0
+    }
+    
+    @IBAction func clearDisplayValue() {
+        if(displayingStringValue.isEmpty) {
+            handleHardClear()
+        }
+        displayingStringValue = "0"
+        valueDisplay.text = displayingStringValue
+        operationValue = 0
+        shouldClear = false
+    }
+    
+    @IBAction func handleHardClear() {
+        displayingStringValue = "0"
+        valueDisplay.text = displayingStringValue
+        operationValue = 0
+        lastOperationValue = 0
+        queuedOperation = operationType.none
+        result = 0
+        shouldClear = false
+    }
+    
+    @IBAction func handleOperatorButtonPressed(_ sender: UIButton) {
+        
+        //blink
+        if queuedOperation != operationType.none {
+            handleProcess()
+        }
+        if let pressedButton = sender.titleLabel?.text {
+            switch pressedButton {
+            case "+":
+                queuedOperation = operationType.sum
+            case "-":
+                queuedOperation = operationType.substract
+            case "x":
+                queuedOperation = operationType.multiply
+            case "/":
+                queuedOperation = operationType.divide
+            default:
+                return
+            }
+        }
+        lastOperationValue = Double(valueDisplay.text ?? "") ?? 0
+        shouldClear = true
+    }
+    
+    @IBAction func handleProcess() {
+        var result: Double = 0
+        switch queuedOperation {
+        case operationType.sum:
+            result = sumValues(lastOperationValue, operationValue)
+        case operationType.substract:
+            result = substractValues(lastOperationValue, operationValue)
+        case operationType.multiply:
+            result = multiplyValues(lastOperationValue, operationValue)
+        case operationType.divide:
+            result = divideValues(lastOperationValue, operationValue)
+        default:
+            result = 0
+        }
+        shouldClear = true
+        displayingStringValue = result.description
+        valueDisplay.text = removeTrailingZeroes(result)
+        lastOperationValue = result
         
     }
     
+    //    OPERATION FUNCTIONS
+    func sumValues(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    func substractValues(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    func multiplyValues(_ a: Double, _ b: Double) -> Double {
+        return (a * b).rounded()
+    }
+    
+    func divideValues(_ a: Double, _ b: Double) -> Double {
+        return a / b
+    }
+    
+    func removeTrailingZeroes(_ value: Double) -> String {
+        let textValue: String = String(value)
+        let splitValues = textValue.split(separator: ".")
+        
+        if Double(splitValues[1]) ?? 0 > 0 { //decimal part
+            return value.description
+        } else {
+            return splitValues[0].description
+        }
+    }
+    
+    func renderValue(_ value: Double) {
+        
+    }
 }
 
